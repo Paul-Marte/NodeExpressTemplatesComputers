c04dac16c6e26407171bd79a1065d6927b4346a4
Paul-Marte <jmarte01@mynbcc.ca>
2024-05-15 20:45:10 -0300

    Initial Submission

---
+++ src/app.js
@@ -0,0 +1,62 @@
+const path = require('path');
+const express = require('express');
+const hbs = require('hbs');
+const product = require('./product');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+// Define paths for Express config
+const publicDirPath = express.static(path.join(__dirname, ' ../public'))
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialPath = path.join(__dirname, '../templates/partials');
+
+// Set up handlebars engine and views location
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+
+//Register partials
+hbs.registerPartials(partialPath);
+
+hbs.registerHelper('toLowerCase', str => {
+    return str.toLowerCase();
+});
+
+// Set up static dir to serve
+app.use(publicDirPath);
+
+app.get('', (req, res) => {
+
+    let data = {
+        title: 'Home',
+        manufacturers: product.getManufactures(),
+        inventory: product.getInventory(),
+        year: new Date().getFullYear()
+    }
+
+    res.render('default', data);
+});
+
+app.get('/:manfacturer', (req, res) => {
+    let data = {
+        title: req.params.manfacturer.charAt(0).toUpperCase() + req.params.manfacturer. slice(1),
+        manufacturers: product.getManufactures(),
+        inventory: product.getModels(req.params.manfacturer),
+        year: new Date().getFullYear()
+    }
+
+    res.render('default', data);
+
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is up on port ${PORT}.`)
+})
+
+
+
+
+
+
+
+
---
+++ src/product.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+
+const getInventory = () => {
+    try {
+        const dataBuffer = fs.readFileSync('computers.json');
+
+        return JSON.parse(dataBuffer.toString());
+    } catch (err) {
+        console. log(err);
+        return [];
+    }
+}
+
+
+const getModels = (manufacturer) => {
+    const inventory = getInventory();
+    const queryManufacturer = inventory.filter(i => i.manufacturer.toLowerCase() === manufacturer.toLowerCase());
+
+    return queryManufacturer;
+}
+
+const getManufactures = () => {
+    const inventory = getInventory();
+    let manufacturers = [];
+
+    inventory.forEach(m => {
+        manufacturers.push(m.manufacturer);
+    });
+
+    return manufacturers;
+}
+
+module.exports = {
+    getInventory,
+    getModels,
+    getManufactures
+}
